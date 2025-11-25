31b3fcccc3ebfe1c77a9dc26ef0ecf85ccc22b97
Archit18th <archit@18thdigitech.net>
2025-11-25 23:23:01 +0530

    pinecone index changed

--- lib/pinecone.ts
+++ lib/pinecone.ts
@@ -4,4 +4,4 @@ export const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY!,
 });
 
-export const pineconeIndex = pinecone.index(process.env.PINECONE_INDEX!).namespace("chat");
+export const pineconeIndex = pinecone.index(process.env.PINECONE_INDEX!);
